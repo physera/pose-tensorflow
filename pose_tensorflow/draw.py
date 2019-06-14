1100fbf6b47b051c80b4811071c6e787339dad1a
Venky Iyer <indigoviolet@gmail.com>
2019-06-13 17:47:31 -0700

    working with draw annotations in nb; added some flags to help visualize

--- pose_tensorflow/draw.py
+++ pose_tensorflow/draw.py
@@ -8,18 +8,19 @@ def find_coords(results, name, side):
     return None
 
 
-def draw_annotations(image, annotations):
+def draw_annotations(image, annotations, confidence_threshold=0.7):
     im = Image.open(image)
     width, height = im.size
     draw = ImageDraw.Draw(im)
 
     for r in annotations:
+        fill = (255, 0, 0) if r["score"] >= confidence_threshold else (128, 128, 128)
         draw.ellipse(
             [
                 (r["x"] * width - 3, r["y"] * height - 3),
                 (r["x"] * width + 3, r["y"] * height + 3),
             ],
-            fill=(255, 0, 0),
+            fill=fill
         )
 
     lines = [
@@ -43,9 +44,17 @@ def draw_annotations(image, annotations):
     for start, end in lines:
         x1, y1 = find_coords(annotations, start[0], start[1])
         x2, y2 = find_coords(annotations, end[0], end[1])
+
+        if start[1] == 1 and end[1] == 1:
+            color = (255, 255, 0)  # yellow
+        elif start[1] == 0 and end[1] == 0:
+            color = (0, 255, 0)  # green
+        else:
+            color = (255, 0, 0)  # red
+
         draw.line(
             [(x1 * width, y1 * height), (x2 * width, y2 * height)],
-            fill=(255, 0, 0),
+            fill=color
         )
 
     del draw
