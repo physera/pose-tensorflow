1f16d227e5be621270aeb0381a6dff9f872cbe10
Venky Iyer <indigoviolet@gmail.com>
2019-07-09 18:57:22 -0700

    working pose drawing on mobile with posenet

--- rn/pose/App.tsx
+++ rn/pose/App.tsx
@@ -1,23 +1,112 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Platform } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Platform, Button } from 'react-native';
 import Tflite from 'tflite-react-native';
 import ImagePicker from 'react-native-image-picker';
 import HTML from 'react-native-render-html';
+import Svg, { Circle, Line } from 'react-native-svg';
+import ImageOverlay from 'react-native-image-overlay';
 
-const blue = '#25d5fd';
-
-const modelFile = 'mobilenet_v1_1.0_224.tflite';
-const labelsFile = 'mobilenet_v1_1.0_224.txt';
+const modelFile = 'posenet_mv1_075_float_from_checkpoints.tflite';
+const labelsFile = '';
 
 let tflite = new Tflite();
 
-interface Props {}
-interface State {
-  msg: string | object | null;
-}
+type Keypoint = {
+  x: number,
+  y: number,
+  part: string,
+  score: number,
+};
+type PoseT = {
+  score: number,
+  keypoints: {
+    [k: string]: Keypoint,
+  },
+};
+type ImageT = { path: string, width: number, height: number };
+type Props = {};
+type State = {
+  msg: string | object | null,
+  image: ImageT | null,
+  poses: [PoseT] | null,
+};
+
+const reindexPoseByPart = (pose: PoseT): PoseT => {
+  let reindexedKps = {};
+  for (const kp of Object.values(pose.keypoints)) {
+    reindexedKps[kp.part] = kp;
+  }
+  return {
+    score: pose.score,
+    keypoints: reindexedKps,
+  };
+};
+
+type Side = 'Left' | 'Right' | 'Across';
+
+const Skeleton: [string, string, Side][] = [
+  ['leftShoulder', 'rightShoulder', 'Across'],
+  ['leftShoulder', 'leftElbow', 'Left'],
+  ['rightShoulder', 'rightElbow', 'Right'],
+  ['leftElbow', 'leftWrist', 'Left'],
+  ['rightElbow', 'rightWrist', 'Right'],
+  ['leftShoulder', 'leftHip', 'Left'],
+  ['rightShoulder', 'rightHip', 'Right'],
+  ['leftHip', 'leftKnee', 'Left'],
+  ['rightHip', 'rightKnee', 'Right'],
+  ['leftKnee', 'leftAnkle', 'Left'],
+  ['rightKnee', 'rightAnkle', 'Right'],
+];
+
+const Pose: React.FunctionComponent<{ pose: PoseT, image: ImageT }> = ({ pose, image }) => {
+  pose = reindexPoseByPart(pose);
+  const points = Object.values(pose.keypoints).map((kp: Keypoint) => {
+    return (
+      <Circle cx={kp.x * image.width} cy={kp.y * image.height} r="5" fill="pink" key={kp.part} />
+    );
+  });
+  const lines = Skeleton.map(([from_part, to_part, side]) => {
+    const from_kp = pose.keypoints[from_part];
+    const to_kp = pose.keypoints[to_part];
+    let strokeColor: string;
+    switch (side) {
+      case 'Left':
+        strokeColor = 'yellow';
+        break;
+      case 'Right':
+        strokeColor = 'green';
+        break;
+      case 'Across':
+        strokeColor = 'red';
+        break;
+    }
+    return (
+      <Line
+        x1={from_kp.x * image.width}
+        y1={from_kp.y * image.height}
+        x2={to_kp.x * image.width}
+        y2={to_kp.y * image.height}
+        stroke={strokeColor}
+        strokeWidth="2"
+        key={`${from_part}-${to_part}`}
+      />
+    );
+  });
+
+  return (
+    <Svg
+      width="100%"
+      height="100%"
+      viewBox={`0 0 ${image.width} ${image.height}`}
+      preserveAspectRatio="none">
+      {points}
+      {lines}
+    </Svg>
+  );
+};
 
 export default class App extends React.Component<Props, State> {
-  state = { msg: null };
+  state = { msg: null, image: null, poses: null };
   constructor(props: Props) {
     super(props);
     tflite.loadModel({ model: modelFile, labels: labelsFile });
@@ -31,17 +120,15 @@ export default class App extends React.Component<Props, State> {
         this.log('Error');
       } else {
         var path = Platform.OS === 'ios' ? response.uri : 'file://' + response.path;
-        tflite.runModelOnImage(
+        this.setState({ image: { path: path, width: response.width, height: response.height } });
+        tflite.runPoseNetOnImage(
           {
             path,
-            imageMean: 128.0,
-            imageStd: 128.0,
-            numResults: 3,
-            threshold: 0.05,
+            threshold: 0.3,
           },
           (err, res) => {
             if (err) this.log(err);
-            else this.log(res);
+            else this.setState({ poses: res });
           }
         );
       }
@@ -54,17 +141,34 @@ export default class App extends React.Component<Props, State> {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={{ marginBottom: 25 }}>Tflite demo</Text>
-        <TouchableOpacity style={styles.button} onPress={this.onSelectImage}>
-          <Text>Select Image</Text>
-        </TouchableOpacity>
-        {this.state.msg ? (
-          <View style={{ margin: 20, borderWidth: 1, borderColor: 'black' }}>
-            <HTML html={`<pre>${JSON.stringify([this.state.msg], null, 2)}</pre>`} />
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={{ marginBottom: 25 }}>Pose demo</Text>
+          <Button title="Select Image" onPress={this.onSelectImage} />
+          <View style={{ marginTop: 25 }}>
+            {this.state.image ? (
+              <ImageOverlay
+                height={this.state.image.height}
+                source={{ uri: this.state.image.path }}
+                contentPosition="top">
+                {this.state.poses ? (
+                  <Pose pose={this.state.poses[0]} image={this.state.image} />
+                ) : null}
+              </ImageOverlay>
+            ) : null}
+            {this.state.poses ? (
+              <View style={{ margin: 20, borderWidth: 1, borderColor: 'red' }}>
+                <HTML html={`<pre>${JSON.stringify(this.state.poses, null, 2)}</pre>`} />
+              </View>
+            ) : null}
+            {this.state.msg ? (
+              <View style={{ margin: 20, borderWidth: 1, borderColor: 'black' }}>
+                <HTML html={`<pre>${JSON.stringify([this.state.msg], null, 2)}</pre>`} />
+              </View>
+            ) : null}
           </View>
-        ) : null}
-      </View>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -75,14 +179,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  button: {
-    width: 200,
-    backgroundColor: blue,
-    borderRadius: 10,
-    height: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 10,
   },
 });
--- rn/pose/android/app/src/main/java/com/pose/MainApplication.java
+++ rn/pose/android/app/src/main/java/com/pose/MainApplication.java
@@ -3,8 +3,9 @@ package com.pose;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
+import com.horcrux.svg.SvgPackage;
 import com.reactlibrary.TfliteReactNativePackage;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ImagePickerPackage(),
-            new TfliteReactNativePackage()
+            new SvgPackage(),
+            new TfliteReactNativePackage(),
+            new ImagePickerPackage()
       );
     }
 
